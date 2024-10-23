4ab7fa9a7ec9058e288458fa00e0a49c2913e78c
Rogério Silva <rgerios@live.com>
2024-10-23 09:41:55 -0300

    Ajuste Providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.72.1"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"  # Região utilizada para o deploy 
+  # Configuration options
 }
