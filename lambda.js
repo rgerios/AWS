5e3da3a486637deaea157bb130631af87a5f79ad
Rogério Silva <39138292+rgerios@users.noreply.github.com>
2024-10-25 02:44:38 -0300

    Correção Lambda-Postgresql

--- lambda.js
+++ lambda.js
@@ -1,26 +1,48 @@
-const mysql = require('mysql');
+const { Client } = require('pg');
 
 exports.handler = async (event) => {
-    // Conectando ao RDS
-    const connection = mysql.createConnection({
+    // Criando uma nova instância do cliente PostgreSQL
+    const client = new Client({
         host: process.env.RDS_HOST,
         user: process.env.RDS_USER,
         password: process.env.RDS_PASSWORD,
-        database: process.env.RDS_DB
+        database: process.env.RDS_DB,
+        port: process.env.RDS_PORT || 5432, // Porto padrão do PostgreSQL
     });
 
-    return new Promise((resolve, reject) => {
-        connection.query('SELECT name FROM users WHERE id = ?', [event.userId], (error, results) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve({
-                    statusCode: 200,
-                    body: JSON.stringify({
-                        username: results[0].name
-                    })
-                });
-            }
-        });
-    });
+    try {
+        // Conectando ao RDS
+        await client.connect();
+
+        // Realizando a consulta
+        const res = await client.query('SELECT name FROM users WHERE id = $1', [event.userId]);
+
+        // Verificando se há resultados
+        if (res.rows.length > 0) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    username: res.rows[0].name
+                }),
+            };
+        } else {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({
+                    message: 'User not found',
+                }),
+            };
+        }
+    } catch (error) {
+        console.error('Error executing query', error.stack);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Internal Server Error',
+            }),
+        };
+    } finally {
+        // Fechando a conexão
+        await client.end();
+    }
 };
